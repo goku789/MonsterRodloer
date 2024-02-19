93e2d094824a658964c234195a753c466b05a690
Neha Deokar <officialadesh@gmail.com>
2024-02-19 08:46:10 +0530

    MonsterRoadler

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends Component {
+
+constructor(props) {
+  super(props)
+
+  this.state = {
+    monsteres:[],
+  }
 }
 
-export default App;
+componentDidMount(){
+  fetch('https://jsonplaceholder.typicode.com/users')
+  .then((response) => response.json())
+  .then((users)=>
+  this.setState(
+    ()=>{
+      return {monsteres:users};
+    },
+    ()=>{
+      console.log(this.state);
+    }
+  )
+  )
+}
+
+
+  render() {
+    return (
+      <div>
+        <input 
+        className='search-boxx' 
+        type='search' 
+        placeholder='search monsters' 
+        onChange={(event) =>{
+          console.log(event.target.value);
+          const arraydata=this.state.monsteres.filter(x=>x.name==event.target.value)
+          console.log(arraydata);
+        }}></input>
+
+        {
+          this.state.monsteres.map((monster)=>{
+            return(
+              <div key={monster.id}>
+                <h1>{monster.name}</h1>
+              </div>
+            )
+          })
+        }
+      </div>
+    )
+  }
+}
+
+export default App
